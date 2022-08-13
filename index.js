3be3e00416c31171f744ce291b10fb6ad69fbb95
Shakib Mia <smdshakibmia2001@gmail.com>
2022-08-13 22:04:27 +0600

    JWT added

--- index.js
+++ index.js
@@ -7,26 +7,56 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 5000;
+
+const jwt = require("jsonwebtoken");
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader?.split(" ")[1] === "null") {
+    return res.status(401).send({ message: "Token Doesn't Match" });
+  }
+
+  next();
+};
 
 async function run() {
   try {
     await client.connect();
 
     const collection = client.db("portfolio").collection("projects");
+    const usersCollection = client.db("portfolio").collection("users");
 
     app.get("/", (req, res) => {
       res.send("JWT BE running");
     });
 
-    app.get("/projects", async (req, res) => {
+    app.get("/projects", verifyJWT, async (req, res) => {
       const query = {};
       const cursor = collection.find(query);
       const projects = await cursor.toArray();
       res.send(projects);
+    });
+
+    app.get("/users", async (req, res) => {
+      const users = usersCollection.find({});
+      res.send(users.toArray());
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
     });
   } finally {
   }
